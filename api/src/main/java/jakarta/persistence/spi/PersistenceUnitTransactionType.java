701faeb2342e3088cac3724248a35673aecdf015
Piotrek Zygielo <piotr@zygielo.pl>
2020-02-08 11:24:10 +0100

    Remove unused imports
    Signed-off-by: Piotrek Zygielo <piotr@zygielo.pl>

--- api/src/main/java/jakarta/persistence/spi/PersistenceUnitTransactionType.java
+++ api/src/main/java/jakarta/persistence/spi/PersistenceUnitTransactionType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -15,8 +15,6 @@
 //     Linda DeMichiel - 2.0
 
 package jakarta.persistence.spi;
-
-import jakarta.persistence.EntityManagerFactory;
 
 /**
  * Specifies whether entity managers created by the {@link
