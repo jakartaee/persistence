1ae01ae3793d129bf921e9b1fe893bed744065d8
Lukas Jungmann <lukas.jungmann@oracle.com>
2021-04-26 10:18:19 +0200

    #77: EntityManager(Factory) should implement AutoCloseable (#312)
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- api/src/main/java/jakarta/persistence/EntityManager.java
+++ api/src/main/java/jakarta/persistence/EntityManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -51,7 +51,7 @@ import jakarta.persistence.criteria.CriteriaDelete;
  * 
  * @since 1.0
  */
-public interface EntityManager {
+public interface EntityManager extends AutoCloseable {
 
     /**
      * Make an instance managed and persistent.
