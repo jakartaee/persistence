2a332fc93fc27fa59317053aa2c77ab0a6110146
Lukas Jungmann <lukas.jungmann@oracle.com>
2021-04-27 13:15:18 +0200

    #264: avoid transitive dependency on java.instrument

---
+++ api/src/main/java/jakarta/persistence/spi/TransformerException.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0,
+ * or the Eclipse Distribution License v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+ */
+package jakarta.persistence.spi;
+
+/**
+ *
+ * @since 3.1
+ */
+public class TransformerException extends Exception {
+
+    /**
+     * Constructs a new <code>TransformerException</code> exception
+     * with <code>null</code> as its detail message.
+     */
+    public TransformerException() {
+        super();
+    }
+
+    /**
+     * Constructs a new <code>TransformerException</code> exception
+     * with the specified detail message.
+     *
+     * @param message the detail message.
+     */
+    public TransformerException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new <code>TransformerException</code> exception
+     * with the specified detail message and cause.
+     *
+     * @param message the detail message.
+     * @param cause the cause.
+     */
+    public TransformerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new <code>TransformerException</code> exception
+     * with the specified cause.
+     *
+     * @param cause the cause.
+     */
+    public TransformerException(Throwable cause) {
+        super(cause);
+    }
+}
