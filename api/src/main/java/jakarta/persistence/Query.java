3c3a7345911cdc1bde0512a5b1a812e5d5dffc14
Gavin King <gavin@hibernate.org>
2022-09-09 09:28:48 +0200

    add Query.getSingleResultOrNull
    see #298

--- api/src/main/java/jakarta/persistence/Query.java
+++ api/src/main/java/jakarta/persistence/Query.java
@@ -11,6 +11,7 @@
  */
 
 // Contributors:
+//     Gavin King      - 3.2
 //     Lukas Jungmann  - 2.2
 //     Linda DeMichiel - 2.1
 //     Linda DeMichiel - 2.0
@@ -110,6 +111,28 @@ public interface Query {
      *         is rolled back 
      */
     Object getSingleResult();
+
+    /**
+     * Execute a SELECT query that returns a single untyped result.
+     * @return the result, or null if there is no result
+     * @throws NonUniqueResultException if more than one result
+     * @throws IllegalStateException if called for a Jakarta
+     *         Persistence query language UPDATE or DELETE statement
+     * @throws QueryTimeoutException if the query execution exceeds
+     *         the query timeout value set and only the statement is
+     *         rolled back
+     * @throws TransactionRequiredException if a lock mode other than
+     *         <code>NONE</code> has been set and there is no transaction
+     *         or the persistence context has not been joined to the transaction
+     * @throws PessimisticLockException if pessimistic locking
+     *         fails and the transaction is rolled back
+     * @throws LockTimeoutException if pessimistic locking
+     *         fails and only the statement is rolled back
+     * @throws PersistenceException if the query execution exceeds
+     *         the query timeout value set and the transaction
+     *         is rolled back
+     */
+    Object getSingleResultOrNull();
 
     /**
      * Execute an update or delete statement.
