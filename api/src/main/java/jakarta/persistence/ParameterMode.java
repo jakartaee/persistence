9cca8e24321037b7fb48d8deee8626f613ce98a3
Lukas Jungmann <lukas.jungmann@oracle.com>
2023-10-27 23:09:35 +0200

    #458: update orm schema for 3.2
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- api/src/main/java/jakarta/persistence/ParameterMode.java
+++ api/src/main/java/jakarta/persistence/ParameterMode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -46,6 +46,6 @@ public enum ParameterMode {
      * <p>Some databases use REF_CURSOR parameters to return result
      * sets from stored procedures.
      */
-    REF_CURSOR,
+    REF_CURSOR
 
 }
