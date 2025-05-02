dbcc797125dd31fe6ac226f313bc0acae41666b7
Gavin King <gavin@hibernate.org>
2025-05-02 22:26:35 +0200

    add Javadoc and try using sealed interfaces

--- api/src/main/java/jakarta/persistence/sql/ColumnMapping.java
+++ api/src/main/java/jakarta/persistence/sql/ColumnMapping.java
@@ -1,13 +1,23 @@
 package jakarta.persistence.sql;
 
-public record ColumnMapping<T>(String name, Class<T> type)
+/**
+ * Maps a column of a JDBC {@link java.sql.ResultSet} to a scalar
+ * value in the result returned by the query.
+ *
+ * @param columnName The name of the mapped column of the result set
+ * @param type The Java type of the resulting scalar value
+ * @param <T> The type of the resulting scalar value
+ *
+ * @since 4.0
+ */
+public record ColumnMapping<T>(String columnName, Class<T> type)
         implements MappingElement<T>, ResultSetMapping<T> {
 
-    public static ColumnMapping<Object> of(String name) {
-        return new ColumnMapping<>(name, Object.class);
+    public static ColumnMapping<Object> of(String columnName) {
+        return new ColumnMapping<>(columnName, Object.class);
     }
 
-    public static <T> ColumnMapping<T> of(String name, Class<T> type) {
-        return new ColumnMapping<>(name, type);
+    public static <T> ColumnMapping<T> of(String columnName, Class<T> type) {
+        return new ColumnMapping<>(columnName, type);
     }
 }
