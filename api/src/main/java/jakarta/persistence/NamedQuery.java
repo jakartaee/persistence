cbb5127aa7f53d874829c37290b31258fecd7d71
Gavin King <gavin@hibernate.org>
2025-11-22 16:00:04 +0100

    add/fix @since

--- api/src/main/java/jakarta/persistence/NamedQuery.java
+++ api/src/main/java/jakarta/persistence/NamedQuery.java
@@ -120,7 +120,7 @@ public @interface NamedQuery {
     /**
      * (Optional) The pessimistic lock scope to use in query execution
      * if a pessimistic lock mode is specified via {@link #lockMode}.
-     * @since 7.0
+     * @since 4.0
      */
     PessimisticLockScope lockScope() default PessimisticLockScope.NORMAL;
 
