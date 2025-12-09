c3accb5a438fd4ab3cacabdfb39960011ba04105
Gavin King <gavin@hibernate.org>
2025-12-09 15:32:21 +0100

    Eliminate @NativeQueryMapping
    and merge its role into pre-existing @SqlResultSetMapping
    
    refresh the very old Javadoc for @SqlResultSetMapping

--- api/src/main/java/jakarta/persistence/SqlResultSetMapping.java
+++ api/src/main/java/jakarta/persistence/SqlResultSetMapping.java
@@ -20,38 +20,70 @@ package jakarta.persistence;
 import java.lang.annotation.Repeatable;
 import java.lang.annotation.Target;
 import java.lang.annotation.Retention;
+
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /** 
  * Specifies a mapping of the columns of a result set of a native SQL query
- * or stored procedure.
+ * or stored procedure. Every SQL result set mapping has a {@link #name},
+ * which may be defaulted, especially when the annotation is applied at
+ * the method level. SQL result set mapping names must be unique within
+ * a persistence unit.
  *
- * <p>Example:
+ * <p>In this example, a named mapping is declared by annotating an entity
+ * class, and a reference to the mapping is obtained from the static
+ * metamodel class of the entity:
  * {@snippet :
- * Query q = em.createNativeQuery(
- *     "SELECT o.id AS order_id, " +
- *         "o.quantity AS order_quantity, " +
- *         "o.item AS order_item, " +
- *         "i.name AS item_name, " +
- *       "FROM Order o, Item i " +
- *       "WHERE (order_quantity &gt; 25) AND (order_item = i.id)",
- *     "OrderResults");
- *
  * @SqlResultSetMapping(
- *     name = "OrderResults",
- *     entities = {
- *         @EntityResult(
- *             entityClass = com.acme.Order.class,
+ *         name = "orderResults",
+ *         entities = @EntityResult(
+ *             entityClass = Order.class,
  *             fields = {
  *                 @FieldResult(name = "id", column = "order_id"),
- *                 @FieldResult(name = "quantity", column = "order_quantity"),
+ *                 @FieldResult(name = "total", column = "order_total"),
  *                 @FieldResult(name = "item", column = "order_item")
- *             })
- *     },
- *     columns = {
- *         @ColumnResult(name = "item_name")
- *     })
+ *             }
+ *         ),
+ *         columns = @ColumnResult(name = "item_name")
+ * )
+ * @Entity
+ * class Order { ... }
+ * }
+ * {@snippet :
+ * Query query = entityManager.createNativeQuery(
+ *         """
+ *           SELECT o.id AS order_id,
+ *                  o.total AS order_total,
+ *                  o.item_id AS order_item,
+ *                  i.desc_name AS item_name
+ *           FROM orders o, order_items i
+ *           WHERE order_quantity > 25 AND order_item = i.id
+ *         """,
+ *         Order_.MAPPING_ORDER_RESULTS
+ * );
+ * }
+ *
+ * <p>In this example, a mapping is specified by annotating a method
+ * which declares a {@link jakarta.persistence.query.StaticNativeQuery},
+ * and the mapping does not need to explicitly specify its name:
+ * {@snippet :
+ * @StaticNativeQuery("SELECT * FROM orders WHERE order_quantity > :?1")
+ * @SqlResultSetMapping(
+ *     entities = @EntityResult(
+ *             entityClass = Order.class,
+ *             fields = {
+ *                 @FieldResult(name = "id", column = "order_id"),
+ *                 @FieldResult(name = "total", column = "order_total"),
+ *                 @FieldResult(name = "item_id", column = "order_item")
+ *             }
+ *     )
+ * )
+ * List<Order> largeOrders(int threshold) {
+ *     return entityManager.createQuery(Shop_.largeOrders(threshold))
+ *             .getResultList();
+ * }
  * }
  *
  * @see Query
@@ -62,16 +94,26 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * @since 1.0
  */
 @Repeatable(SqlResultSetMappings.class)
-@Target(TYPE)
+@Target({TYPE, METHOD})
 @Retention(RUNTIME)
 public @interface SqlResultSetMapping { 
 
     /** 
      * The name given to the result set mapping, and used to
      * refer to it in the methods of the {@link Query} and
-     * {@link StoredProcedureQuery} APIs.
+     * {@link StoredProcedureQuery} APIs. If not specified,
+     * the name is assigned a default value depending on
+     * where the annotation occurs.
+     * <ul>
+     * <li>If the annotation occurs on a type, the name
+     * defaults to the unqualified name of the type.
+     * <li>Otherwise, if the annotation occurs on a member
+     * of a type, the name defaults to the concatenation of
+     * the unqualified name of the type, with the string
+     * {@code "."}, and the name of the annotated member.
+     * </ul>
      */
-    String name(); 
+    String name() default "";
 
     /**
      * Specifies the result set mapping to entities.
