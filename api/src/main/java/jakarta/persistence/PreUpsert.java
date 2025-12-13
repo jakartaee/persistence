30bba4647d26805d3a7ada451f6e1128bdbadb89
Gavin King <gavin@hibernate.org>
2025-12-13 12:51:03 +0100

    Specifies -> Declares
    it's just a marker; it doesn't "specify" anything

--- api/src/main/java/jakarta/persistence/PreUpsert.java
+++ api/src/main/java/jakarta/persistence/PreUpsert.java
@@ -22,7 +22,7 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Specifies a callback method for the corresponding lifecycle event.
+ * Declares a callback method for the corresponding lifecycle event.
  * This annotation may be applied to methods of an entity class, a
  * mapped superclass, or a callback listener class.
  *
