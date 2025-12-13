77705762fe750334ff23da8e797ba29d3f86dff2
Gavin King <gavin@hibernate.org>
2025-12-13 16:04:05 +0100

    add a way to exclude a column from schema validation
    sometimes users run into a case where the schema validation
    is too strict, and their program actually works as is; it's
    nice to have a way to exclude a column from validation
    
    see #815

--- api/src/main/java/jakarta/persistence/OrderColumn.java
+++ api/src/main/java/jakarta/persistence/OrderColumn.java
@@ -109,4 +109,15 @@ public @interface OrderColumn {
      * @since 3.2
      */
     String options() default "";
+
+    /**
+     * Determines if this column is validated as part of the
+     * schema {@linkplain SchemaManager#validate validation}
+     * process. By default, the column is validated. If the
+     * column should be excluded from validation, specify
+     * {@code validated=false}.
+     *
+     * @since 4.0
+     */
+    boolean validated() default true;
 }
