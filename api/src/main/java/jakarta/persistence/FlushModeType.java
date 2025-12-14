0fd0885de1776555c2869492b669e3e3534e0874
Gavin King <gavin@hibernate.org>
2025-12-14 12:29:12 +0100

    add FlushModeType.EXPLICIT

--- api/src/main/java/jakarta/persistence/FlushModeType.java
+++ api/src/main/java/jakarta/persistence/FlushModeType.java
@@ -21,7 +21,7 @@ package jakarta.persistence;
  *
  * <p>When a query is executed within a transaction:
  * <ul>
- * <li>If {@link #AUTO} is set via the {@link Query Query} or
+ * <li>If {@link #AUTO} is selected via the {@link Query Query} or
  *     {@link TypedQuery} object, or if the flush mode setting for
  *     the persistence context is {@code AUTO} (the default) and a
  *     flush mode setting has not been specified for the {@code Query}
@@ -32,9 +32,10 @@ package jakarta.persistence;
  *     of the query. The persistence provider implementation might
  *     guarantee this by flushing pending updates to modified
  *     entities to the database before executing the query.
- * <li>On the other hand, if {@link #COMMIT} is set, the effect of
- *     updates made to entities in the persistence context on query
- *     results is unspecified.
+ * <li>On the other hand, if {@link #COMMIT} or {@link #EXPLICIT}
+ *     is selected, the effect on query results of modifications
+ *     made to entities held in the persistence context is
+ *     unspecified.
  * </ul>
  *
  * <p>If there is no transaction active or if the persistence context
@@ -47,15 +48,35 @@ package jakarta.persistence;
  * @since 1.0
  */
 public enum FlushModeType {
+    /**
+     * Every flush is an explicit operation requested by the application
+     * program. The session is never automatically flushed. Modifications
+     * to entities held in the persistence context might not be visible
+     * to the processing of queries and might never be made persistent.
+     *
+     * @since 4.0
+     */
+    EXPLICIT,
 
     /**
-     * Flushing to occur at transaction commit. The provider may flush
-     * at other times, but is not required to.
+     * The persistence context is flushed automatically before any attempt
+     * to commit the transaction with which it is associated. The provider
+     * may flush at other times, but is not required to. Modifications to
+     * entities held in the persistence context might not be visible to
+     * the processing of queries.
      */
-   COMMIT,
+    COMMIT,
 
     /**
-     * (Default) Flushing to occur at query execution.
+     * A flush might occur before execution of any query, especially if
+     * the flush is necessary to ensure consistency between the results
+     * of the query and modifications held in memory. The provider is not
+     * required to flush before execution of the query, but is required to
+     * ensure that the effect of every modification to the state of every
+     * entity associated with the persistence context is visible to the
+     * processing of the query.
+     * <p>
+     * This is the default flush mode for a newly created entity manager.
      */
-   AUTO
+    AUTO
 }
