599c48264a2961c9cd93b17b0978b0ee7f1f62df
Gavin King <gavin@hibernate.org>
2025-12-19 11:06:07 +0100

    Apply suggestions from code review
    Co-authored-by: Nathan Rauh <nathan.rauh@us.ibm.com>

--- api/src/main/java/jakarta/persistence/PersistenceAgents.java
+++ api/src/main/java/jakarta/persistence/PersistenceAgents.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -11,8 +11,7 @@
  */
 
 // Contributors:
-//     Linda DeMichiel - 2.1
-//     Linda DeMichiel - 2.0
+//     Gavin King - 4.0
 
 package jakarta.persistence;
 
@@ -32,7 +31,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 public @interface PersistenceAgents {
 
-    /** (Required) One or more {@link PersistenceUnit} annotations. */
+    /** (Required) One or more {@link PersistenceAgent} annotations. */
     PersistenceAgent[] value();
 
 }
