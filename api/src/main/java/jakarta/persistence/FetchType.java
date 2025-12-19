95e28d6ef0b0e1ebc182b179c3ed8d051c774e24
Gavin King <gavin@hibernate.org>
2025-12-19 21:37:41 +0100

    more documentation for default fetch type

--- api/src/main/java/jakarta/persistence/FetchType.java
+++ api/src/main/java/jakarta/persistence/FetchType.java
@@ -31,13 +31,39 @@ package jakarta.persistence;
  *     entity. The implementation is permitted to eagerly fetch
  *     data for which the {@code LAZY} strategy hint has been
  *     specified.
+ * <li>A {@link #DEFAULT} value specifies that the fetching
+ *     strategy depends on the <em>default fetch type of the
+ *     persistence unit for one-to-one and many-to-one
+ *     associations</em>. This default fetch type is
+ *     controlled via the {@code default-fetch-type} element in
+ *     the {@code persistence.xml} file or by setting the
+ *     {@link PersistenceConfiguration#defaultFetchType()
+ *     defaultFetchType} of the {@link PersistenceConfiguration},
+ *     and defaults to {@link FetchType#EAGER} for backward
+ *     compatibility.
  * </ul>
  *
- * <p>Example:
+ * <p>This example specifies that a {@link ManyToOne} association
+ * should, by default, be fetched lazily on first access:
  * {@snippet :
- * @Basic(fetch = LAZY)
- * protected String getName() { return name; }
+ * @ManyToOne(fetch = LAZY)
+ * private Publisher publisher;
  * }
+ * <p>This example specifies that an {@link ElementCollection} is
+ * fetched eagerly:
+ * {@snippet :
+ * @ElementCollection(fetch = EAGER)
+ * Set<String> topics;
+ * }
+ * <p>The use of {@code fetch = EAGER} often leads to fetching of
+ * unwanted data, and is therefore discouraged. Eager fetching
+ * may be explicitly requested precisely where needed using an
+ * {@link EntityGraph} or a query with {@code left join fetch}.
+ *
+ * @apiNote It is very strongly recommended that new applications
+ * set the {@linkplain #DEFAULT default fetch type} for one-to-one
+ * and many-to-one associations to {@link FetchType#LAZY} to avoid
+ * accidental fetching of unnecessary data.
  *
  * @see Basic
  * @see ElementCollection
@@ -63,6 +89,10 @@ public enum FetchType {
      * Specifies that the fetching strategy depends on
      * the default fetch type of the persistence unit
      * for one-to-one and many-to-one associations.
+     * @apiNote It is very strongly recommended that
+     * new applications set the default fetch type to
+     * {@link FetchType#LAZY} to avoid accidental
+     * fetching of unnecessary data.
      * @since 4.0
      */
     DEFAULT
