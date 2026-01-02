92c37df12511cd5ce9342ab9d4020d76be8c92fe
Gavin King <gavin@hibernate.org>
2026-01-02 17:50:31 +0100

    header updates

--- api/src/main/java/jakarta/persistence/OneToOne.java
+++ api/src/main/java/jakarta/persistence/OneToOne.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2023 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2025 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -11,6 +11,7 @@
  */
 
 // Contributors:
+//     Gavin King      - 4.0
 //     Linda DeMichiel - 2.1
 //     Linda DeMichiel - 2.0
 
