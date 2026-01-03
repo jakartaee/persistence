688236c8037f0de37ad7f5c1dcf942d4c61c61cf
Gavin King <gavin@hibernate.org>
2026-01-03 12:12:02 +0100

    update header

--- api/src/main/java/jakarta/persistence/criteria/CriteriaBuilder.java
+++ api/src/main/java/jakarta/persistence/criteria/CriteriaBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2023 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2026 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -11,6 +11,7 @@
  */
 
 // Contributors:
+//     Gavin King      - 4.0
 //     Gavin King      - 3.2
 //     Linda DeMichiel - 2.1
 //     Linda DeMichiel - 2.0
