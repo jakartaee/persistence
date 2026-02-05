384133afd3c9837bbab71c8bcb786d6aa66e46b4
Gavin King <gavin@hibernate.org>
2026-02-05 16:46:58 +0100

    introduce @EntityListener annotation
    see #784

--- api/src/main/java/jakarta/persistence/EntityListener.java
+++ api/src/main/java/jakarta/persistence/EntityListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2025 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -11,39 +11,42 @@
  */
 
 // Contributors:
-//     Linda DeMichiel - 2.1
-//     Linda DeMichiel - 2.0
-
-
+//     Gavin King      - 4.0
 package jakarta.persistence;
 
-import java.lang.annotation.Target;
 import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Specifies the entity listener classes associated with the
- * annotated class. This annotation may be applied to an
- * {@linkplain Entity entity class} or to a
- * {@linkplain MappedSuperclass mapped superclass}.
- *
- * <p>This annotation is an alternative to {@link EntityListener}.
+ * Declares an entity listener class and specifies the entity
+ * classes to which it applies. The entity listener receives
+ * entity lifecycle callbacks for every entity class belonging
+ * to the persistence unit and assignable to one of its declared
+ * entity lifecycle callback methods. It is not necessary to
+ * separately declare the entity classes to which the listener
+ * applies.
+ * <p>
+ * This annotation is an alternative to {@link EntityListeners}.
  * Callback methods of listeners declared using this annotation
- * are invoked after callback methods of listeners declared
- * using {@link EntityListener}.
- *
- * <p>Every entity listener class must have a public constructor
+ * are invoked before callback methods of listeners declared
+ * using {@link EntityListeners}. However, there is no defined
+ * ordering among listeners declared using this annotation. If
+ * the order of invocation of callback methods is important,
+ * the entity listeners should be declared using the
+ * {@link EntityListeners} annotation instead.
+ * <p>
+ * Every entity listener class must have a public constructor
  * with no parameters.
  * {@snippet :
- * @Entity
- * @EntityListeners(BookObserver.class)
- * class Book { ... }
+ * @EntityListener(Book.class)
+ * class BookObserver { ... }
  * }
- *
- * <p>The specified entity listener classes may have callback
- * methods annotated with any of the standard lifecycle callback
- * annotations:
+ * <p>
+ * The annotated class may have callback methods annotated with
+ * any of the standard lifecycle callback annotations:
  * <ul>
  * <li>{@link PostLoad},
  * <li>{@link PrePersist}, {@link PostPersist},
@@ -52,7 +55,8 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  *     {@link PostUpdate}, {@link PreUpsert}, {@link PostUpsert},
  *     {@link PreDelete}, and {@link PostDelete}.
  * </ul>
- * <p>A callback method declared by an entity listener class must
+ * <p>
+ * A callback method declared by an entity listener class must
  * have the signature {@code void method(E entity)} where {@code E}
  * is an entity class, a mapped superclass, or a supertype of the
  * entity class or mapped superclass to which the entity listener
@@ -60,6 +64,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * {@code E}, the callback method is invoked for any such class to
  * which the entity listener applies.
  * {@snippet :
+ * @EntityListener
  * class BookObserver {
  *
  *     @PostPersist
@@ -69,11 +74,12 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  *
  * }
  * }
- *
- * <p>An entity listener class may have multiple callback methods
- * for a given type of lifecycle event, but at most one callback
- * method for a given type of event and given parameter type.
+ * <p>
+ * An entity listener class may have multiple callback methods for
+ * a given type of lifecycle event, but at most one callback method
+ * for a given type of event and given parameter type.
  * {@snippet :
+ * @EntityListener
  * class Observer {
  *
  *     // called only for Books
@@ -96,8 +102,8 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  *
  * }
  * }
- *
- * <p>Entity listener classes in Jakarta EE environments support
+ * <p>
+ * Entity listener classes in Jakarta EE environments support
  * dependency injection through the Contexts and Dependency
  * Injection (CDI) API when CDI is enabled. An entity listener
  * class that makes use of CDI injection may also define lifecycle
@@ -106,16 +112,11 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * dependencies have been injected and before the entity listener
  * instance is destroyed, respectively.
  *
- * @since 1.0
+ *
+ * @since 4.0
  */
 @Target(TYPE)
 @Retention(RUNTIME)
-public @interface EntityListeners {
-
-    /**
-     * The callback listener classes for the annotated entity,
-     * in the order in which their lifecycle callback methods
-     * are invoked.
-     */
-    Class<?>[] value();
+@Discoverable
+public @interface EntityListener {
 }
