9867bb25ee3ba8d3f650296fd4906d87a30cc67d
Gavin King <gavin@hibernate.org>
2026-02-25 12:18:55 +0100

    simplify definition of QueryFlushMode semantics

--- api/src/main/java/jakarta/persistence/QueryFlushMode.java
+++ api/src/main/java/jakarta/persistence/QueryFlushMode.java
@@ -39,8 +39,8 @@ public enum QueryFlushMode {
 	 * modifications to the database before executing the query.
 	 * Pending modifications to entities held in the persistence
 	 * context are always visible to the processing of the query.
-	 *
-	 * @see EntityManager#flush()
+	 * The {@linkplain EntityManager#getFlushMode current flush
+	 * mode} of the persistence context is completely overridden.
 	 */
 	FLUSH,
 
@@ -49,20 +49,14 @@ public enum QueryFlushMode {
 	 * modifications to the database before executing the query.
 	 * Pending modifications to entities held in the persistence
 	 * context might not be visible to the processing of the query.
-	 * <p>
-	 * When the current {@linkplain EntityManager#getFlushMode
-	 * flush mode} of the persistence context is
-	 * {@link FlushModeType#EXPLICIT}, the persistence provider
-	 * is not permitted to flush pending modifications before
-	 * executing a query with {@code NO_FLUSH}. Otherwise, this
-	 * mode is considered a hint to the persistence provider.
-	 * <p>
-	 * Equivalent to:
 	 * <ul>
-	 * <li>{@link FlushModeType#EXPLICIT} if the current flush
-	 *     mode of the perssistence context is {@code EXPLICIT},
-	 *     or
-	 * <li>{@link FlushModeType#COMMIT} otherwise.
+	 * <li>If the {@linkplain EntityManager#getFlushMode current
+	 *     flush mode} of the persistence context is
+	 *     {@link FlushModeType#EXPLICIT}, the persistence provider
+	 *     is not permitted to flush pending modifications before
+	 *     executing the query.
+	 * <li>Otherwise, this mode is considered a hint to the
+	 *     persistence provider.
 	 * </ul>
 	 */
 	NO_FLUSH,
@@ -70,22 +64,8 @@ public enum QueryFlushMode {
 	/**
 	 * The persistence provider must determine whether to flush
 	 * before executing the query by considering the current
-	 * {@linkplain EntityManager#getFlushMode flush mode} of
-	 * the persistence context.
-	 * <p>
-	 * If the current flush mode is {@link FlushModeType#AUTO},
-	 * the persistence provider must ensure that every modification
-	 * to the state of every entity associated with the persistence
-	 * context which could possibly affect the result of the query
-	 * is visible to the processing of the query. The persistence
-	 * provider implementation might guarantee this by flushing
-	 * pending modifications to the database before executing the
-	 * query.
-	 * <p>
-	 * Equivalent to the current flush mode of the persistence
-	 * context.
-	 *
-	 * @see EntityManager#getFlushMode()
+	 * {@linkplain EntityManager#getFlushMode flush mode} of the
+	 * persistence context.
 	 */
 	DEFAULT
 }
