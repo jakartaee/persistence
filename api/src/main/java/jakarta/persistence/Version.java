3c7141e43a00869d6ef5be9719eb3ec113c48fb1
Nathan Rauh <nathan.rauh@us.ibm.com>
2026-05-12 11:00:13 -0500

    review comment to centralize information on automatic version updates to the Version annotation

--- api/src/main/java/jakarta/persistence/Version.java
+++ api/src/main/java/jakarta/persistence/Version.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2023 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2026 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -35,6 +35,9 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * or timestamp held in the database changes between reading the
  * state of an entity instance and attempting to update or delete
  * the state of the instance.
+ *
+ * <p>The entity version must be updated by the persistence provider
+ * each time the state of an entity instance is written to the database.
  *
  * <p>The version attribute must be of one of the following basic
  * types: {@code int}, {@link Integer}, {@code short}, {@link Short},
