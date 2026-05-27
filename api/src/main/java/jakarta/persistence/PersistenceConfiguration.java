97eb60c9fc7bbb6233418d8972e945c94084db3e
Gavin King <gavin@hibernate.org>
2026-05-27 14:40:36 +0200

    clean up an unused import

--- api/src/main/java/jakarta/persistence/PersistenceConfiguration.java
+++ api/src/main/java/jakarta/persistence/PersistenceConfiguration.java
@@ -24,7 +24,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import static java.util.Objects.requireNonNull;
 
